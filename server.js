e90cc2947b5d24981f6c08baea106ca5fdeea7f4
apa-narola <ap@vpninfotech.com>
2018-07-10 16:38:30 +0530

    deploy heroku v1

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
+var http = require('http');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('dist'));
+// app.use(express.static('dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/*', function(req,res) {
+/*app.get('/*', function(req,res) {
     
 res.sendFile(path.join('/dist/index.html'));
+});*/
+
+app.get('/*', function(req, res) {
+    res.sendFile(__dirname + '/dist/index.html');
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+//app.listen(process.env.PORT || 8080);
+
+//Set Port
+const port = process.env.PORT || 8080;
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Running on localhost:${port}`));
