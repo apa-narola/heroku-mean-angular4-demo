da7132e5fb7aff251974900d9a20909386f85d88
apa-narola <ap@vpninfotech.com>
2018-07-10 17:33:07 +0530

    deploy heroku v10

--- server/server.js
+++ server/server.js
@@ -5,17 +5,12 @@ var http = require('http');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '../dist'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'../dist/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
-
-//app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, '../dist/index2.html')) ) ;
-
-// Start the app by listening on the default Heroku port
-//app.listen(process.env.PORT || 8080);
 
 //Set Port
 const port = process.env.PORT || 8080;
